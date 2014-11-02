8d6024256bb2f3ee2bc2e1c00c501125818487e3
Arnaud Kapp <kapp.arno@gmail.com>
2014-11-02 15:19:39 +0100

    Process tree

--- system_status.sh
+++ system_status.sh
@@ -2,9 +2,11 @@
 #
 # Send various info
 
+set -e
+
 TMP_DIR=$(mktemp -d)
 MESSAGE_FILE=$TMP_DIR/message_file
-
+PROCESS_TREE_FILE=$TMP_DIR/process_tree
 
 function btrfs_info()
 {
@@ -34,11 +36,20 @@ function load_info()
 
 echo -e "General system report, ran `date`.\n" >> $MESSAGE_FILE
 
+function process_tree()
+{
+    echo "Current process tree (detailed process tree is available as an attachment):" >> $MESSAGE_FILE
+    pstree -un >> $MESSAGE_FILE
+
+    pstree -acun > $PROCESS_TREE_FILE
+}
+
 btrfs_info
 load_info
+process_tree
 
 source config.sh
 
 ( ./secure_mail.sh -r=${ADMIN_MAIL} -rk=${ADMIN_PGP_KEY} -f=${SOURCE_MAIL} \
     -b=$MESSAGE_FILE -s="System Status Script Report" \
-    --passphrase=$SIGNING_KEY_PASSPHRASE --assume-yes) || fail "Mail send error"
+    --passphrase=$SIGNING_KEY_PASSPHRASE --assume-yes -- $PROCESS_TREE_FILE) || fail "Mail send error"
