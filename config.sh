cfe2dc0c0b1b43618ed901b5467c382d065d4def
Arnaud Kapp <kapp.arno@gmail.com>
2014-11-07 16:51:10 +0100

    more config options

--- config.sh
+++ config.sh
@@ -7,3 +7,11 @@ ADMIN_MAIL=''
 ADMIN_PGP_KEY=''
 SOURCE_MAIL=''
 SIGNING_KEY_PASSPHRASE=''
+
+NICE_LEVEL=20
+
+## Ionice info. Either idle or ignored
+IONICE_LEVEL=IDLE
+
+## Use the ssmtp binary to send report mail.
+USE_SSMTP='1'
